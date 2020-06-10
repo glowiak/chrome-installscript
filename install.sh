84e63b050a6585bf507eacca7e56faeb100d46ea
glowiak <52356948+glowiak@users.noreply.github.com>
2020-06-10 14:28:11 +0200

    ewfwqq3
    regrfdg

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-apt-get install curl
-curl https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_70.0.3538.77-1_amd64.deb
+wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_70.0.3538.77-1_amd64.deb
 dpkg --install google-chrome-stable_70.0.3538.77-1_amd64.deb
